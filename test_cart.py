c552ca5687a763a9286bf02a6d931eddabb4dec2
Ramprasath2725 <ramprasath2725@gmail.com>
2026-03-10 14:37:33 +0530

    Update test_cart.py
    Signed-off-by: Ramprasath2725 <ramprasath2725@gmail.com>

--- test_cart.py
+++ test_cart.py
@@ -1,3 +1,12 @@
-print("cart test")
-print ("abc")
-print("sddf")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.google.com")
+print("Google opened successfully")
+
+time.sleep(3)
+driver.quit()
